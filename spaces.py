d098fdeeb80f02c632c6d1fa241bf367fead30a6
Mohit Pradhan <159414367+TsuKKi-is-dead@users.noreply.github.com>
2026-05-19 16:19:35 +0530

    Spaces was learned

---
+++ spaces.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import matplotlib.pyplot as plt 
+
+
+img = cv.imread('NewYork.jpg')
+cv.imshow('New York', img)
+
+plt.imshow(img)
+plt.show()
+# # Convert to grayscale
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+# #BGR to HSV
+# hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+# cv.imshow('HSV', hsv)
+
+# #BGR to LAB
+# lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+# cv.imshow('LAB', lab)
+
+# cv.waitKey(0)  
+# cv.destroyAllWindows()
